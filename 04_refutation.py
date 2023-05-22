1a39dc81da3e3de484ee84d4f7c58271510b587a
lmorosdb <lmorosdb>
2023-05-22 23:42:25 +0000

    Uncommenting long running refutation tests

--- 04_refutation.py
+++ 04_refutation.py
@@ -38,29 +38,29 @@ refutation_random_common_cause_df
 
 # COMMAND ----------
 
-# import mlflow
+import mlflow
  
-# mlflow.autolog(disable=True)
+mlflow.autolog(disable=True)
 
-# res_unobserved_common_cause = model.refute_estimate(
-#     estimand=estimand,
-#     estimate=estimate,
-#     show_progress_bar=True,
-#     method_name="add_unobserved_common_cause",
-#     confounders_effect_on_treatment="binary_flip",
-#     confounders_effect_on_outcome="linear",
-#     effect_fraction_on_treatment=0.05,
-#     effect_fraction_on_outcome=0.05,
-# )
+res_unobserved_common_cause = model.refute_estimate(
+    estimand=estimand,
+    estimate=estimate,
+    show_progress_bar=True,
+    method_name="add_unobserved_common_cause",
+    confounders_effect_on_treatment="binary_flip",
+    confounders_effect_on_outcome="linear",
+    effect_fraction_on_treatment=0.05,
+    effect_fraction_on_outcome=0.05,
+)
 
-# refutation_unobserved_common_cause_df = pd.DataFrame([{
-#         "Refutation Type": res_unobserved_common_cause.refutation_type,
-#         "Estimated Effect": res_unobserved_common_cause.estimated_effect,
-#         "New Effect": res_unobserved_common_cause.new_effect,
-#         "Refutation Result (p value)": None 
-#     }])
+refutation_unobserved_common_cause_df = pd.DataFrame([{
+        "Refutation Type": res_unobserved_common_cause.refutation_type,
+        "Estimated Effect": res_unobserved_common_cause.estimated_effect,
+        "New Effect": res_unobserved_common_cause.new_effect,
+        "Refutation Result (p value)": None 
+    }])
 
-# refutation_unobserved_common_cause_df  
+refutation_unobserved_common_cause_df  
 
 # COMMAND ----------
 
@@ -106,42 +106,42 @@ refutation_subset_df
 
 # COMMAND ----------
 
-# import mlflow
-# mlflow.autolog(disable=True)
+import mlflow
+mlflow.autolog(disable=True)
 
-# coefficients = np.array([10, 0.02])
-# bias = 1000
-
-
-# def linear_gen(df):
-#     y_new = np.dot(df[["W0", "W1"]].values, coefficients) + bias
-#     return y_new
+coefficients = np.array([10, 0.02])
+bias = 1000
 
 
-# res_dummy_outcome = model.refute_estimate(
-#     estimand=estimand,
-#     estimate=estimate,
-#     show_progress_bar=True,
-#     method_name="dummy_outcome_refuter",
-#     outcome_function=linear_gen,
-# )[0]
+def linear_gen(df):
+    y_new = np.dot(df[["W0", "W1"]].values, coefficients) + bias
+    return y_new
 
-# refutation_dummy_outcome_df = pd.DataFrame([{
-#         "Refutation Type": res_dummy_outcome.refutation_type,
-#         "Estimated Effect": res_dummy_outcome.estimated_effect,
-#         "New Effect": res_dummy_outcome.new_effect,
-#         "Refutation Result (p value)": res_dummy_outcome.refutation_result["p_value"] 
-#     }])
 
-# refutation_dummy_outcome_df  
+res_dummy_outcome = model.refute_estimate(
+    estimand=estimand,
+    estimate=estimate,
+    show_progress_bar=True,
+    method_name="dummy_outcome_refuter",
+    outcome_function=linear_gen,
+)[0]
+
+refutation_dummy_outcome_df = pd.DataFrame([{
+        "Refutation Type": res_dummy_outcome.refutation_type,
+        "Estimated Effect": res_dummy_outcome.estimated_effect,
+        "New Effect": res_dummy_outcome.new_effect,
+        "Refutation Result (p value)": res_dummy_outcome.refutation_result["p_value"] 
+    }])
+
+refutation_dummy_outcome_df  
 
 # COMMAND ----------
 
 refutation_df = pd.concat([
         refutation_random_common_cause_df,
-        #refutation_unobserved_common_cause_df,
+        refutation_unobserved_common_cause_df,
         refutation_subset_df,
         refutation_placebo_df,
-        #refutation_dummy_outcome_df,
+        refutation_dummy_outcome_df,
     ])
 refutation_df
